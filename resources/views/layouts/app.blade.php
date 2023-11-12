237431636775ead2b07a2075e93e916ae2ce64db
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2023-11-12 13:32:21 +0100

    Add layout file for the application with navigation and footer sections.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100">
+    <nav class="bg-white shadow">
+        <div class="container mx-auto px-4">
+            <div class="flex justify-between items-center py-4">
+                <div>
+                    <a href="/" class="text-lg font-semibold text-slate-900">NWS</a>
+                </div>
+                <div>
+                    <a href="{{ route('students.index') }}" class="text-slate-900 hover:text-slate-700">Etudiants</a>
+                </div>
+    
+            </div>
+        </div>
+    </nav>
+
+    <main class="container mx-auto px-4 py-8">
+        @yield('content')
+    </main>
+
+    <footer class="bg-white shadow">
+        <div class="container mx-auto px-4 py-4">
+            <div class="text-center text-gray-500">
+                &copy; {{ date('Y') }} Mon site. Tous droits réservés.
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
