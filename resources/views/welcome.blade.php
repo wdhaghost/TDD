50533882c50b65c18bcdd42f4a78bf723cab9088
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2023-11-12 15:19:36 +0100

    Update welcome page with new inventory and equipment links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,27 +5,27 @@
 @section('content')
     <div class="container mx-auto mt-5">
         <h1 class="text-3xl font-semibold mb-4">Bienvenue à la Normandie Web School</h1>
-        <p class="text-lg mb-4">La meilleure école pour l'apprentissage du développement web.</p>
+        <p class="text-lg mb-4">Inventaire NWS</p>
         
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div class="bg-white p-6 rounded-lg shadow-md">
-                <h2 class="text-xl font-semibold mb-2">Liste des Étudiants</h2>
+                <h2 class="text-xl font-semibold mb-2">Étudiants</h2>
                 <ul>
                     <li><a href="{{ route('students.index') }}" class="text-blue-600 hover:text-blue-900">Liste des Étudiants</a></li>
-                    <li>  <a href="{{ route('students.create') }}" class="bg-green-500 hover:bg-green-600 text-white font-semibold px-4 py-2 rounded shadow">Ajouter du matériel</a>    </li>
+                    <li>  <a href="{{ route('students.create') }}" class="text-blue-600 hover:text-blue-900">Ajouter du matériel</a>    </li>
                 </ul>
                 
             </div>
-            <div class="bg-white p-6 rounded-lg shadow-md">
-                <h2 class="text-xl font-semibold mb-2">Liste de matériels</h2>
+            <div class="flex flex-col bg-white p-6 rounded-lg shadow-md">
+                <h2 class="text-xl font-semibold mb-2">Matériels</h2>
                 <ul>
-                    <li><a href="{{ route('equipments.index') }}" class="text-blue-600 hover:text-blue-900">Liste des Étudiants</a></li>
-                    <li>  <a href="{{ route('equipments.create') }}" class="bg-green-500 hover:bg-green-600 text-white font-semibold px-4 py-2 rounded shadow">Ajouter du matériel</a>    </li>
+                    <li><a href="{{ route('equipments.index') }}" class="text-blue-600 hover:text-blue-900">Liste de matériel</a></li>
                 </ul>
+                 <a href="{{ route('equipments.create') }}" class="text-blue-600 hover:text-blue-900">Ajouter du matériel</a>   
             </div>
 
             <div class="bg-white p-6 rounded-lg shadow-md">
-                <h2 class="text-xl font-semibold mb-2">Listes de prêts</h2>
+                <h2 class="text-xl font-semibold mb-2">Prêts</h2>
                
             </div>
         </div>
